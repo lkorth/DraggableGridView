499014313aa1d61caa65cb5873e9ff4b7777b9bb
lkorth <luke@lukekorth.com>
2012-08-14 09:56:01 -0500

    Update param for changeDataForCell and whitespace changes

--- src/com/animoto/android/dgv/DraggableGridViewCell.java
+++ src/com/animoto/android/dgv/DraggableGridViewCell.java
@@ -1,20 +1,20 @@
 package com.animoto.android.dgv;
 
 public interface DraggableGridViewCell {
-	
-	//Should return the position of the cell in the underlying data. 
+
+	//Should return the position of the cell in the underlying data.
 	public int getPositionInData() throws CellDataNotSetException;
-	
+
 	public class CellDataNotSetException extends Exception {
 		public CellDataNotSetException(String msg) {
 			super(msg);
 		}
 	}
-	
+
 	public String getConvertIdentifier();
-	
-	
-	public boolean changeDataForCell(Object obj);
-	
-	
+
+
+	public boolean changeDataForCell(int number);
+
+
 }
