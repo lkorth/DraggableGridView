abaa13f98fc313eb5b2351e1c04efe13ad56bf49
lkorth <luke@lukekorth.com>
2012-08-14 10:07:03 -0500

    Update adapter

--- src/com/animoto/android/dgvSample/DgvDatabaseAdapter.java
+++ src/com/animoto/android/dgvSample/DgvDatabaseAdapter.java
@@ -1,72 +1,88 @@
 package com.animoto.android.dgvSample;
 
-
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Hashtable;
-
-import com.animoto.android.db.ORMHelper;
-import com.animoto.android.db.Photo;
-import com.animoto.android.dgv.DraggableGridViewAdapter;
-import com.animoto.android.dgv.DraggableGridViewCell;
-import com.animoto.android.dgvdbsample.R;
-
 import android.content.Context;
-import android.graphics.Color;
-import android.util.Log;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.animoto.android.db.DatabaseHelper;
+import com.animoto.android.dgv.DraggableGridViewAdapter;
 
 public class DgvDatabaseAdapter extends DraggableGridViewAdapter {
 
-	public DgvDatabaseAdapter(Context c) {
-		super(c);
+	public static final int[] mImages = {
+		R.drawable.sample_img_1, R.drawable.sample_img_2,
+		R.drawable.sample_img_3, R.drawable.sample_img_4,
+		R.drawable.sample_img_5, R.drawable.sample_img_6,
+		R.drawable.sample_img_7, R.drawable.sample_img_8,
+		R.drawable.sample_img_9, R.drawable.sample_img_10,
+		R.drawable.sample_img_11, R.drawable.sample_img_12,
+		R.drawable.sample_img_13, R.drawable.sample_img_14,
+		R.drawable.sample_img_15, R.drawable.sample_img_16,
+		R.drawable.sample_img_17
+	};
+
+	public static final int[] mCaptions = {
+		R.string.a, R.string.b,
+		R.string.c, R.string.d,
+		R.string.e, R.string.f,
+		R.string.g, R.string.h,
+		R.string.i, R.string.j,
+		R.string.k, R.string.l,
+		R.string.m, R.string.n,
+		R.string.o, R.string.p,
+		R.string.q
+	};
+
+	private Context mContext;
+
+	public DgvDatabaseAdapter(Context context) {
+		super(context);
+		mContext = context;
 	}
-	
+
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		
-			try {
-				return (int)ORMHelper.photoDao.countOf(); //@TODO - more robust casting here. 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return 0;
-			}
-
+		return mImages.length;
 	}
-	
+
 	@Override
 	public Object getItem(int position) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public long getItemId(int position) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		Photo p = ORMHelper.photoDao.getPhotoWithPosition(position);
-		
-		CustomPhotoCell cellView = (CustomPhotoCell)this.getConvertibleCell(CustomPhotoCell.CUSTOM_PHOTO_CELL_IDENTIFIER);
-		if (cellView == null) cellView = (CustomPhotoCell)inflater.inflate(R.layout.photo_cell, null);
-		else {
-			//Log.i("dgv", "Will be recycling cell with old position " + cellView.getPhoto().position + " in to new cell with position " + p.position);
+		DatabaseHelper dbh = new DatabaseHelper(mContext);
+		SQLiteDatabase db = dbh.getReadableDatabase();
+		Cursor cursor = db.query(DatabaseHelper.icons, new String[] { DatabaseHelper.iconNumber }, DatabaseHelper.position + " =?",
+				new String[]{ Integer.toString(position) }, null, null, null);
+		cursor.moveToNext();
+		int item = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.iconNumber));
+
+		System.out.println("position: " + position + " icon: " + item);
+
+		Icon icon = (Icon) this.getConvertibleCell(Icon.name);
+
+		if(icon == null){
+			LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			icon = (Icon) layoutInflater.inflate(R.layout.photo_cell, null);
 		}
-		if (p != null) cellView.setPhoto(p);
-		
-		return cellView;
+
+		icon.setIcon(position);
+
+		db.close();
+		dbh.close();
+
+		return icon;
 	}
 
 }
+
