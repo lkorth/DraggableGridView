e6113eef7124e877767fe33dede0699076f7e2ec
lkorth <luke@lukekorth.com>
2012-10-12 15:48:12 -0500

    Compress redundant code

--- src/com/animoto/android/db/DatabaseHelper.java
+++ src/com/animoto/android/db/DatabaseHelper.java
@@ -29,90 +29,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		db.execSQL("CREATE TABLE " + icons + " (" + iconNumber + " INTEGER PRIMARY KEY, " +
 				position + " INTEGER)");
 
-		ContentValues cv = new ContentValues();
-		cv.put(position, 0);
-		cv.put(iconNumber, 0);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 1);
-		cv.put(iconNumber, 1);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 2);
-		cv.put(iconNumber, 2);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 3);
-		cv.put(iconNumber, 3);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 4);
-		cv.put(iconNumber, 4);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 5);
-		cv.put(iconNumber, 5);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 6);
-		cv.put(iconNumber, 6);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 7);
-		cv.put(iconNumber, 7);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 8);
-		cv.put(iconNumber, 8);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 9);
-		cv.put(iconNumber, 9);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 10);
-		cv.put(iconNumber, 10);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 11);
-		cv.put(iconNumber, 11);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 12);
-		cv.put(iconNumber, 12);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 13);
-		cv.put(iconNumber, 13);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 14);
-		cv.put(iconNumber, 14);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 15);
-		cv.put(iconNumber, 15);
-		db.insert(icons, null, cv);
-
-		cv = new ContentValues();
-		cv.put(position, 16);
-		cv.put(iconNumber, 16);
-		db.insert(icons, null, cv);
+		ContentValues cv;
+		for(int i = 0; i < 17; i++){
+			cv = new ContentValues();
+			cv.put(position, i);
+			cv.put(iconNumber, i);
+			db.insert(icons, null, cv);
+		}
 	}
 
 	@Override
