7d3c6a435f7ba14dcf0cf7abd82c4f92c0db35c8
lkorth <luke@lukekorth.com>
2012-10-12 22:17:50 -0500

    Remove unnecessary code, refactor and clean up

--- src/com/animoto/android/dgv/DraggableGridViewCell.java
+++ src/com/animoto/android/dgv/DraggableGridViewCell.java
@@ -2,19 +2,15 @@ package com.animoto.android.dgv;
 
 public interface DraggableGridViewCell {
 
-	//Should return the position of the cell in the underlying data.
-	public int getPositionInData() throws CellDataNotSetException;
+    //Should return the position of the cell in the underlying data.
+    public int getPositionInData() throws CellDataNotSetException;
 
-	public class CellDataNotSetException extends Exception {
-		public CellDataNotSetException(String msg) {
-			super(msg);
-		}
-	}
+    public class CellDataNotSetException extends Exception {
+        public CellDataNotSetException(String msg) {
+            super(msg);
+        }
+    }
 
-	public String getConvertIdentifier();
-
-
-	public boolean changeDataForCell(int number);
-
+    public boolean changeDataForCell(int number);
 
 }
--- src/com/animoto/android/dgvSample/Icon.java
+++ src/com/animoto/android/dgvSample/Icon.java
@@ -10,45 +10,40 @@ import com.animoto.android.dgv.DraggableGridViewCell;
 
 public class Icon extends LinearLayout implements DraggableGridViewCell {
 
-	public static final String name = "ICON";
+    public static final String name = "ICON";
 
-	private TextView mCaption;
-	private ImageView mImage;
-	private int currentPosition = -1;
+    private TextView mCaption;
+    private ImageView mImage;
+    private int currentPosition = -1;
 
-	public Icon(Context context, AttributeSet attr) {
-		super(context, attr);
-	}
+    public Icon(Context context, AttributeSet attr) {
+        super(context, attr);
+    }
 
-	@Override
-	protected void onFinishInflate() {
-		super.onFinishInflate();
-		mCaption = (TextView) this.findViewById(R.id.grid_item_text);
-		mImage = (ImageView) this.findViewById(R.id.grid_item_image);
-	}
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mCaption = (TextView) this.findViewById(R.id.grid_item_text);
+        mImage = (ImageView) this.findViewById(R.id.grid_item_image);
+    }
 
-	@Override
-	public boolean changeDataForCell(int number) {
-		setIcon(number);
-		return true;
-	}
+    @Override
+    public boolean changeDataForCell(int number) {
+        setIcon(number);
+        return true;
+    }
 
-	public void setIcon(int number){
-		currentPosition = number;
-		mCaption.setText(DraggableAdapter.mCaptions[number]);
-		mImage.setImageResource(DraggableAdapter.mImages[number]);
-	}
+    public void setIcon(int number){
+        currentPosition = number;
+        mCaption.setText(DraggableGridViewAdapter.mCaptions[number]);
+        mImage.setImageResource(DraggableGridViewAdapter.mImages[number]);
+    }
 
-	@Override
-	public int getPositionInData() throws CellDataNotSetException {
-		if (currentPosition == -1) {
-			throw new CellDataNotSetException("Custom photo cell doesn't have an underlying model object set");
-		} else return currentPosition;
-	}
-
-	@Override
-	public String getConvertIdentifier() {
-		return name;
-	}
+    @Override
+    public int getPositionInData() throws CellDataNotSetException {
+        if (currentPosition == -1) {
+            throw new CellDataNotSetException("Custom photo cell doesn't have an underlying model object set");
+        } else return currentPosition;
+    }
 
 }
